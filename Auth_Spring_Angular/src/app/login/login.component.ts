3b034a9a8e48c4b1e3bbf3aee940032d2967a078
kiran9431 <pkiran3194@gmail.com>
2023-08-17 06:31:10 +0400

    added code

--- Auth_Spring_Angular/src/app/login/login.component.ts
+++ Auth_Spring_Angular/src/app/login/login.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements Oninit {
 
+  constructor(){ }
+
+  ngOninit(); void{
+
+  }
+  login(loginForm):{
+    console.log("Form is submitted")
+}
 }
